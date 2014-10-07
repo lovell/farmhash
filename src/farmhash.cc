d62f6b59a164a9c82ff087db2311febc062f9b1e
Lovell Fuller <github@lovell.info>
2014-10-07 12:07:12 +0100

    Use uint32 V8 types where relevant

--- src/farmhash.cc
+++ src/farmhash.cc
@@ -22,15 +22,15 @@ NAN_METHOD(Hash32) {
   NanScope();
   string input = *String::Utf8Value(args[0]->ToString());
   uint32_t hash = Hash32(input);
-  NanReturnValue(NanNew<Number>(hash));
+  NanReturnValue(NanNew<Uint32>(hash));
 }
 
 NAN_METHOD(Hash32WithSeed) {
   NanScope();
   string input = *String::Utf8Value(args[0]->ToString());
-  uint32_t seed = args[1]->Int32Value();
+  uint32_t seed = args[1]->Uint32Value();
   uint32_t hash = Hash32WithSeed(input, seed);
-  NanReturnValue(NanNew<Number>(hash));
+  NanReturnValue(NanNew<Uint32>(hash));
 }
 
 NAN_METHOD(Hash64) {
@@ -43,7 +43,7 @@ NAN_METHOD(Hash64) {
 NAN_METHOD(Hash64WithSeed) {
   NanScope();
   string input = *String::Utf8Value(args[0]->ToString());
-  uint64_t seed = static_cast<uint64_t>(args[1]->Int32Value());
+  uint64_t seed = args[1]->IntegerValue();
   uint64_t hash = Hash64WithSeed(input, seed);
   NanReturnValue(NanNew<String>(Uint64ToString(hash)));
 }
@@ -51,8 +51,8 @@ NAN_METHOD(Hash64WithSeed) {
 NAN_METHOD(Hash64WithSeeds) {
   NanScope();
   string input = *String::Utf8Value(args[0]->ToString());
-  uint64_t seed1 = static_cast<uint64_t>(args[1]->Int32Value());
-  uint64_t seed2 = static_cast<uint64_t>(args[2]->Int32Value());
+  uint64_t seed1 = args[1]->IntegerValue();
+  uint64_t seed2 = args[2]->IntegerValue();
   uint64_t hash = Hash64WithSeeds(input, seed1, seed2);
   NanReturnValue(NanNew<String>(Uint64ToString(hash)));
 }
@@ -63,7 +63,7 @@ NAN_METHOD(Fingerprint32) {
   NanScope();
   string input = *String::Utf8Value(args[0]->ToString());
   uint32_t hash = Fingerprint32(input);
-  NanReturnValue(NanNew<Number>(hash));
+  NanReturnValue(NanNew<Uint32>(hash));
 }
 
 NAN_METHOD(Fingerprint64) {
