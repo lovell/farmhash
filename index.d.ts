0ff54cbad653fe2be41e81cbc3e869b78b2de525
Sunguk Lee <sulee@ea.com>
2024-09-13 00:29:36 +0900

    TypeScript: Fix bigint return type (#54)

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,15 @@
 export function fingerprint32(input: string | Buffer): number;
 
-export function fingerprint64(input: string | Buffer): BigInt;
+export function fingerprint64(input: string | Buffer): bigint;
 
-export function fingerprint64signed(input: string | Buffer): BigInt;
+export function fingerprint64signed(input: string | Buffer): bigint;
 
 export function hash32(input: string | Buffer): number;
 
 export function hash32WithSeed(input: string | Buffer, seed: number): number;
 
-export function hash64(input: string | Buffer): BigInt;
+export function hash64(input: string | Buffer): bigint;
 
-export function hash64WithSeed(input: string | Buffer, seed: number): BigInt;
+export function hash64WithSeed(input: string | Buffer, seed: number): bigint;
 
-export function hash64WithSeeds(input: string | Buffer, seed1: number, seed2: number): BigInt;
+export function hash64WithSeeds(input: string | Buffer, seed1: number, seed2: number): bigint;
